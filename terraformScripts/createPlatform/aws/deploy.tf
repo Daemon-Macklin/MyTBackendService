356a50a7038807f0d53df7b82eea05bb6eda72f8
Daemon-Macklin <daemon.macklin@outlook.com>
2020-01-28 00:35:56 +0000

    AWS creation

--- terraformScripts/createPlatform/aws/deploy.tf
+++ terraformScripts/createPlatform/aws/deploy.tf
@@ -1,14 +1,35 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners = ["063491364108"]
-}
-
 resource "aws_vpc" "my_vpc" {
   cidr_block = "172.16.0.0/16"
+  enable_dns_hostnames = true
 
   tags = {
     Name = "MyTVPC"
   }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id = aws_vpc.my_vpc.id
+  cidr_block = "172.16.10.0/24"
+  availability_zone = "eu-west-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "MyTSubnet"
+  }
+}
+
+resource "aws_route" "route" {
+  route_table_id = aws_vpc.my_vpc.default_route_table_id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.gw.id
 }
 
 resource "aws_security_group" "MyT-SG" {
@@ -20,12 +41,34 @@ resource "aws_security_group" "MyT-SG" {
     from_port = 3000
     protocol = "tcp"
     to_port = 3000
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     from_port = 5672
     protocol = "tcp"
     to_port = 5672
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    protocol = "tcp"
+    to_port = 22
+    cidr_blocks = ["89.101.235.182/32"]
+  }
+
+    egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+    egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    ipv6_cidr_blocks     = ["::/0"]
   }
 }
 
@@ -34,13 +77,13 @@ resource "aws_key_pair" "MyT-Key" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC1czloJeXHAt65uC2n1eWiuzgREygy/C8nGIRYvStElsOa0Hunjw4HMCPz4BEdkO9p2nEkO16QmZbOks6P1eC3LtdbQgBYiy4+s+k/Sg4NlXosN1eI7gTM9CP88iDSVh+gZPracJYfq8EB4eHZBZRQD/EZkuVohrq8TxDuJppNp2xLT7wqkEviooRoxz270cJLQ1ozO89GX437Bkr1+VbzuL9p8l+Bay2FXXStCrvMLq9a8A3bC7oZ6f3zyHyzmFSY7U9yuD+YdMBXhQ0TXNGflB/d2RfJWAfDwgiejuHBJ0M63+kQW7IsoKVrhV9d0JOpZDM5Caek+8ngh/BBn7V/ dmacklin@dmacklin-KPL-W0X"
 }
 
-
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
+  ami = "ami-02df9ea15c1778c9c"
   instance_type = "t3a.small"
   key_name = aws_key_pair.MyT-Key.id
   security_groups = [aws_security_group.MyT-SG.id]
-  associate_public_ip_address = true
+  subnet_id = aws_subnet.my_subnet.id
+  depends_on = ["aws_internet_gateway.gw"]
 
   ebs_block_device {
     device_name = "/dev/xvdb"
--- terraformScripts/createPlatform/openstack/deploy.tf
+++ terraformScripts/createPlatform/openstack/deploy.tf
@@ -7,17 +7,28 @@ resource "openstack_compute_instance_v2" "DMacklinMyTDemo" {
   image_name = "ubuntu-1804-LTS"
   availability_zone = "nova"
   flavor_name = "ig.big.disk"
-  key_pair = "${var.openstack_keypair}"
+  key_pair = var.openstack_keypair
   security_groups = ["default", "ssh-open-world", "ICMP", "http", "3000 - Open to all", "RabbitMq - Open to all"]
   network {
-    name = "${var.tenant_network}"
+    name = var.tenant_network
     }
 }
-
 resource "openstack_compute_floatingip_associate_v2" "myip" {
-  floating_ip = "${openstack_networking_floatingip_v2.myip.address}"
-  instance_id = "${openstack_compute_instance_v2.DMacklinMyTDemo.id}"
-  fixed_ip    = "${openstack_compute_instance_v2.DMacklinMyTDemo.network[0].fixed_ip_v4}"
+  floating_ip = openstack_networking_floatingip_v2.myip.address
+  instance_id = openstack_compute_instance_v2.DMacklinMyTDemo.id
+  fixed_ip    = openstack_compute_instance_v2.DMacklinMyTDemo.network[0].fixed_ip_v4
+}
+
+resource "openstack_blockstorage_volume_v2" "DMacklinMyTDemoVolume" {
+  name        = "DMacklinMyTVolume"
+  description = ""
+  size        = 2
+  availability_zone = "nova"
+}
+
+resource "openstack_compute_volume_attach_v2" "va_1" {
+  instance_id = openstack_compute_instance_v2.DMacklinMyTDemo.id
+  volume_id   = openstack_blockstorage_volume_v2.DMacklinMyTDemoVolume.id
 }
 
 output "instance_ip_addr" {
