3c379d50b9e473e6577a3f5c874e84597584556b
Daemon-Macklin <daemon.macklin@outlook.com>
2020-02-17 20:12:09 +0000

    Adding socketio for cors

--- api.py
+++ api.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 from app import EXPORT_BLUEPRINTS
 
 # Create app
 def createApp():
     app = Flask(__name__)
     app.config.from_pyfile('app/config.py')
+    CORS(app, resources={r"/*": {"origins": "*"}})
 
     for bluePrint in EXPORT_BLUEPRINTS:
         app.register_blueprint(bluePrint)
